5a8185052acb7921e19f09bd2e3f83c749c76d17
whitestorm2346 <andrew770426@gmail.com>
2022-10-19 15:02:46 +0800

    W07-P2

---
+++ js/demo_18.js
@@ -0,0 +1,19 @@
+function showpage(index){
+    const p = document.querySelector('.show-classdemo');
+    console.log('p: ', p);
+
+    switch(index){
+        case 1:
+            p.innerHTML = `<iframe src="./demo/w02/" width="100%" height="100%" />`;
+            break;
+        case 2:
+            p.innerHTML = `<iframe src="./demo/w03/card_18.html" width="100%" height="100%" />`;
+            break;
+        case 3:
+            p.innerHTML = `<iframe src="./demo/w05/blog_18.html" width="100%" height="100%" />`;
+            break;
+        case 4:
+            p.innerHTML = `<iframe src="./demo/w06/blog_18.html" width="100%" height="100%" />`;
+            break;
+    }
+}
